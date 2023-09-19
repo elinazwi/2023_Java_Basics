8bc1fe2bf213506d1066876220a9a8e13ab94adf
elina <e.zwischenbrugger@student.htldornbirn.at>
2023-09-19 09:38:01 +0200

    If_bedingungen_Aufgabe1 started

---
+++ src/at/elina/units/If_bedingungen_Aufgabe1.java
@@ -0,0 +1,22 @@
+package at.elina.units;
+
+import java.util.Random;
+
+public class If_bedingungen_Aufgabe1 {
+    public static void main(String[] args) {
+        // Erstelle eine Zufallszahl zwischen 0 und 100
+        Random random = new Random();
+        int randomNumber = random.nextInt(100);
+
+        // Gib die Zufallszahl aus
+        System.out.println(randomNumber);
+        // Wenn die Zahl kleiner ist als 20  gib aus "Mini"
+        // Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
+        // Wenn die Zahl größer als 50 ist gib aus "Large"
+
+
+    }
+}
+
+
+
