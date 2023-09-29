c45a7b239a3ace33cd540dc53f70a1532a1ec957
elina <e.zwischenbrugger@student.htldornbirn.at>
2023-09-29 08:24:36 +0200

    finished Loops_Aufgabe2

--- src/at/elina/units/Loops_Aufgabe2.java
+++ src/at/elina/units/Loops_Aufgabe2.java
@@ -1,2 +1,18 @@
-package at.elina.units;public class Loops_Aufgabe2 {
+package at.elina.units;
+
+public class Loops_Aufgabe2 {
+
+    public static void main(String[] args) {
+
+//Zähle die geraden Ziffern zwischen 1 und 1000 zusammen -
+//Tip: Starte den Loop bei 2 und erhöhe den Zählindex jeweils um 2.
+
+      int sum = 0;
+
+        for (int i = 2; i <= 1000; i = i + 2) {
+            sum += i;
+        }
+
+        System.out.println("Die Summe aller geraden Ziffern zwischen 1 und 1000 beträgt " + sum);
+    }
 }
