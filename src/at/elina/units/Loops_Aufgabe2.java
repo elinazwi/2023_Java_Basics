479cb163e88c740e7f3873eba87d737e426ff520
elina <e.zwischenbrugger@student.htldornbirn.at>
2023-09-29 08:40:41 +0200

    corrected Loops_Aufgabe2

--- src/at/elina/units/Loops_Aufgabe2.java
+++ src/at/elina/units/Loops_Aufgabe2.java
@@ -9,7 +9,7 @@ public class Loops_Aufgabe2 {
 
       int sum = 0;
 
-        for (int i = 2; i <= 1000; i = i + 2) {
+        for (int i = 2; i <= 1000; i += 2) {
             sum += i;
         }
 
