a9a827e2a1af0c09b5d5d73bad31dab9999b92bd
elina <e.zwischenbrugger@student.htldornbirn.at>
2023-09-29 09:31:30 +0200

    started Bankomat

---
+++ src/at/elina/excercises/Bankomat.java
@@ -0,0 +1,10 @@
+package at.elina.excercises;
+
+import java.util.Scanner;
+
+public class Bankomat {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
