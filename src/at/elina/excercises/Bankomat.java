b4f340b638af798d1ffd9a61d213a6f11bc88897
elina <e.zwischenbrugger@student.htldornbirn.at>
2023-10-02 14:46:05 +0200

    continued Bankomat

--- src/at/elina/excercises/Bankomat.java
+++ src/at/elina/excercises/Bankomat.java
@@ -3,8 +3,36 @@ package at.elina.excercises;
 import java.util.Scanner;
 
 public class Bankomat {
+    double balance = 0.0; // Anfangskontostand
+    boolean isRunning = true;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Wähle den gewünschten Behfehl aus: ");
+        System.out.println("1. Einzahlen");
+        System.out.println("2. Abheben");
+        System.out.println("3. Kontostand anzeigen");
+        System.out.println("4. Beenden");2
+
+        int selectionOfUser = scanner.nextInt();
+        System.out.println(selectionOfUser);
+
+
+        switch (selectionOfUser){
+            case 1:
+                System.out.println("Welchen Betrag möchten sie einzahlen?");
+                break;
+            case 2:
+                System.out.println("Welchen Betrag möchten sie abheben?");
+                break;
+            case 3:
+                System.out.println("Kontostand");
+                break;
+            case 4:
+                System.out.println("Beenden");
+                break;
+
+        }
+
     }
 }
