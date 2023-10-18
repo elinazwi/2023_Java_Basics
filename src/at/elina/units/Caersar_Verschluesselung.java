b3006c3277a59d3991a864004877f2053ca776ea
elina <e.zwischenbrugger@student.htldornbirn.at>
2023-10-18 09:35:04 +0200

    worked on Caesar_Verschluesselung again

--- src/at/elina/units/Caersar_Verschluesselung.java
+++ src/at/elina/units/Caersar_Verschluesselung.java
@@ -1,7 +1,7 @@
 package at.elina.units;
 
 public class Caersar_Verschluesselung {
-    public static String doEncrypt(String data){
+    public static String doEncrypt(String data, int shift){
         char[] chars = data.toCharArray();
         char[] result = new char[chars.length];
 
@@ -9,11 +9,35 @@ public class Caersar_Verschluesselung {
             char actualCharacter = chars[i];
             // zw 65 und 90
             // dann zu 65 dazu zählen
-            actualCharacter += 1;
-            if (actualCharacter==90){
-                actualCharacter=65;
+            actualCharacter += shift;
+
+            if (actualCharacter>122){
+                actualCharacter =  (char)(97 + shift);
+
+            }else if(actualCharacter>90){
+                actualCharacter = (char)(65 + shift);
             }
+
             result[i]=actualCharacter;
+        }
+
+        String output = new String(result);
+        System.out.println(output);
+        return output;
+    }
+
+    public static String doDecrypt(String data,int shift){
+        char[] chars = data.toCharArray();
+        char[] result = new char[chars.length];
+
+        for (int i = 0; i < chars.length; i++) {
+            char encryptedCharacter = chars[i];
+            // zw 65 und 90
+            // dann zu 65 dazu zählen
+            encryptedCharacter -= shift;
+
+
+            result[i]=encryptedCharacter;
         }
 
         String output = new String(result);
--- src/at/elina/units/CeasarMain.java
+++ src/at/elina/units/CeasarMain.java
@@ -3,8 +3,12 @@ package at.elina.units;
 public class CeasarMain    {
     public static void main(String[] args) {
         String text = "az";
+        int shift = 4;
+        System.out.println("Original:" + text);
 
-        String encrypted = Caersar_Verschluesselung.doEncrypt(text);
-        System.out.println(encrypted);
+        String encrypted = Caersar_Verschluesselung.doEncrypt(text,shift);
+        String decrypted = Caersar_Verschluesselung.doDecrypt(encrypted,4);
+        System.out.println("Encrypted with shift " + shift + " - " + encrypted);
+        System.out.println(decrypted);
     }
 }
