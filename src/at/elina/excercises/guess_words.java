99c62cb42778a8d311f5fc3f52269206a088b056
elina <e.zwischenbrugger@student.htldornbirn.at>
2023-10-23 14:45:26 +0200

    started Hangman

---
+++ src/at/elina/excercises/guess_words.java
@@ -0,0 +1,12 @@
+package at.elina.excercises;
+import java.util.Random;
+public class guess_words {
+
+    public static void main(String[] args) {
+        String words = "Deutsch";
+
+
+
+        }
+    }
+}
