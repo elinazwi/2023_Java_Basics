103447b75a9fb8afd67b3c3ecfcbf0d6c5a1e479
elina <e.zwischenbrugger@student.htldornbirn.at>
2023-11-22 09:35:01 +0100

    started with oo (car)

---
+++ src/at/elina/oo/car.java
@@ -0,0 +1,8 @@
+package at.elina.oo;
+
+public class car {
+    public String color;
+    public double fuelConsumption;
+    public String serialNumber;
+
+}
---
+++ src/at/elina/oo/car_main.java
@@ -0,0 +1,14 @@
+package at.elina.oo;
+
+public class car_main {
+    public static void main(String[] args) {
+        car c1 = new car();
+        c1.color = "blue";
+
+        car c2 = new car();
+        c2.color = "pink";
+
+        System.out.println(c2.color);
+    }
+}
+
