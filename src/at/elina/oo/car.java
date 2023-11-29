f236746e991d760af7705f173a3a6ac314391ea1
elina <e.zwischenbrugger@student.htldornbirn.at>
2023-11-29 09:28:05 +0100

    worked on oo car

--- src/at/elina/oo/car.java
+++ src/at/elina/oo/car.java
@@ -2,7 +2,23 @@ package at.elina.oo;
 
 public class car {
     public String color;
-    public double fuelConsumption;
+    public int fuelConsumption;
     public String serialNumber;
+    public String brand;
+    public int fuelAmount;
+    public int tankVolume;
+
+    //Methode
+    public void drive(){
+        this.fuelAmount = this.fuelAmount - fuelConsumption;
+        System.out.println("i am driving");
+    }
+
+    public void brake(){
+        System.out.println("the car is braking");
+    }
+
+    public void turboBoost(){
+    }
 
 }
--- src/at/elina/oo/car_main.java
+++ src/at/elina/oo/car_main.java
@@ -4,11 +4,23 @@ public class car_main {
     public static void main(String[] args) {
         car c1 = new car();
         c1.color = "blue";
+        c1.brand = "Porsche";
+        c1.fuelConsumption = 9;
+        c1. serialNumber = "P1234";
+        c1.fuelAmount = 70;
 
         car c2 = new car();
         c2.color = "pink";
+        c2.brand = "Jaguar";
+        c2.fuelConsumption = 6;
+        c2.serialNumber = "J1234";
+        c2.fuelAmount = 20;
 
-        System.out.println(c2.color);
+        System.out.println(c2.fuelAmount);
+        c2.drive();
+        System.out.println(c2.fuelAmount);
+        c2.brake();
+
     }
 }
 
