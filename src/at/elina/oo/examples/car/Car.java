f1f604119efc6310bb0289c14ce335603178dbc0
elina <e.zwischenbrugger@student.htldornbirn.at>
2024-02-21 08:47:44 +0100

    finished exercise car

--- src/at/elina/oo/examples/car/Car.java
+++ src/at/elina/oo/examples/car/Car.java
@@ -3,18 +3,29 @@ package at.elina.oo.examples.car;
 public class Car {
     private String color;
     public int maximumSpeed;
-    public int basePrice;
+    public double basePrice;
     public double baseConsumption;
     public int distance;
     public int mileage;
+    private Producer producer;
+    private Engine engine;
 
-    public Car(String color, int maximumSpeed, int basePrice, double baseConsumption, int distance, int mileage){
+    public Car(String color, int maximumSpeed, double basePrice, double baseConsumption, int distance, int mileage, Producer producer, Engine engine){
         this.color = color;
         this.maximumSpeed = maximumSpeed;
         this.basePrice = basePrice;
         this.baseConsumption = baseConsumption;
         this.distance = distance;
         this.mileage = mileage;
+        this.producer = producer;
+        this.engine = engine;
+    }
+
+    public void discountedPrice(){
+        double discountOfCar = this.basePrice * producer.getDiscount() /100;
+        double newPrice = basePrice - discountOfCar;
+        System.out.println("The Base price is " + basePrice + "€ and the discounted Price is " + newPrice + "€");
+
     }
 
     public void changeConsumption(){
@@ -40,11 +51,11 @@ public class Car {
         this.maximumSpeed = maximumSpeed;
     }
 
-    public int getBasePrice() {
+    public double getBasePrice() {
         return basePrice;
     }
 
-    public void setBasePrice(int basePrice) {
+    public void setBasePrice(double basePrice) {
         this.basePrice = basePrice;
     }
 
--- src/at/elina/oo/examples/car/Main.java
+++ src/at/elina/oo/examples/car/Main.java
@@ -2,12 +2,14 @@ package at.elina.oo.examples.car;
 
 public class Main {
     public static void main(String[] args){
-        Car car1 = new Car("gold", 250, 100000, 20, 600, 51000);
         Engine e1 = new Engine(Engine.TYPE.Diesel, 300);
         Producer p1 = new Producer("Elina:))", "Austria", 0.8);
+        Car car1 = new Car("gold", 250, 100000, 20, 600, 51000, p1, e1);
         System.out.println("Color of the car: " + car1.getColor());
         System.out.println("Name of Producer: " + p1.getNameOfProducer());
         car1.changeConsumption();
         System.out.println(car1.getBaseConsumption());
+        System.out.println(car1.getBasePrice());
+        car1.discountedPrice();
     }
 }
