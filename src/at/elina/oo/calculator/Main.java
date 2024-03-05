e88cc4d25ae1163a38af8fe8760611170e5a84d7
elina <e.zwischenbrugger@student.htldornbirn.at>
2024-03-05 18:33:23 +0100

    added ScientificCalculator

--- src/at/elina/oo/calculator/Main.java
+++ src/at/elina/oo/calculator/Main.java
@@ -3,10 +3,12 @@ package at.elina.oo.calculator;
 public class Main {
     public static void main(String[] args) {
         BaseCalculator base1 = new BaseCalculator(10, 2);
+        ScientificCalculator s1 = new ScientificCalculator(25, 5);
 
         base1.addition();
         base1.subtraction();
         base1.multiplication();
         base1.division();
+        System.out.println(s1.squareRoot(25));
     }
 }
--- src/at/elina/oo/calculator/ScientificCalculator.java
+++ src/at/elina/oo/calculator/ScientificCalculator.java
@@ -1,4 +1,12 @@
 package at.elina.oo.calculator;
 
-public class ScientificCalculator {
+public class ScientificCalculator extends BaseCalculator{
+
+    public ScientificCalculator(double number1, double number2) {
+        super(number1, number2);
+    }
+
+    public double squareRoot(double number){
+        return Math.sqrt(number);
+    }
 }
